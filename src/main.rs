4026638ef9c8ec5e4ffa9312748aba8baeffac4c
Mukhamed Karanashev <menchauser@gmail.com>
2016-01-06 18:30:41 +0300

    Added options and vagrant configuration

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use getopts::Options;
 use std::env;
 
 fn print_usage(program: &String, opts: Options) {
-    let brief = format!("Usage: {} [options] [URL]", program);
+    let brief = format!("Usage: {} [options]", program);
     print!("{}", opts.usage(&brief));
 }
 
@@ -14,7 +14,14 @@ fn main() {
     let program = args[0].clone();
 
     let mut opts = Options::new();
-    opts.optflag("S", "", "set up repository for package installation");
+    opts.optflag("S", "setup", "set up repository for package installation");
+    opts.optflag("L", "list", "list installed packages");
+    opts.optopt("I", "install", "install package from source URL", "URL");
+    opts.optopt("F", "fetch", "fetch remote source package", "URL");
+    opts.optopt("B", "build", "build fetched package", "URL");
+    opts.optopt("c", "", "configure options", "OPTIONS");
+    opts.optopt("m", "", "make options", "OPTIONS");
+    opts.optopt("i", "", "install target", "TARGET");
     opts.optflag("h", "help", "print this help menu");
 
     let matches = match opts.parse(&args[1..]) {
